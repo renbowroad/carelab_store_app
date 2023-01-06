cc79478613932cab763a4c90a9d9e4467512cc5e
renbowroad <reeen1030@gmail.com>
2023-01-06 12:27:28 +0900

    ログイン機能実装

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,16 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import React from "react";
 import Button from "../components/button";
+import { auth } from "../firebase/client";
 
-const login = () => {
-  return <Button>ログインする</Button>;
+const Login = () => {
+  const login = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider).then((res) => {
+      console.log(res);
+    });
+  };
+  return <Button onClick={login}>ログインする</Button>;
 };
 
-export default login;
+export default Login;
